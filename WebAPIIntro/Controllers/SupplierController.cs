caf9e68ce2b45c8fafd68f0f6e5f8fb545bd788d
tolga <tolga@DESKTOP-C6KH20F>
2024-03-31 12:27:03 +0300

    SupplierUpdate

--- WebAPIIntro/Controllers/SupplierController.cs
+++ WebAPIIntro/Controllers/SupplierController.cs
@@ -30,5 +30,48 @@ namespace WebAPIIntro.Controllers
             return Ok(supplier);
 
         }
-    }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            NorthwndContext ctx = new NorthwndContext();
+            Supplier supplier = ctx.Suppliers.FirstOrDefault(y => y.SupplierId == id);
+           
+            if (supplier == null) 
+            {
+                return NotFound("Böyle bir Supplier bulunamadı");
+            }
+
+            ctx.Suppliers.Remove(supplier);
+            ctx.SaveChanges();
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Add(Supplier supplier) 
+        {
+            NorthwndContext context = new NorthwndContext();
+            context.Suppliers.Add(supplier);
+            context.SaveChanges();
+            return Ok();
+  
+        }
+        [HttpPut]
+        public IActionResult Update(Supplier supplier) 
+        {
+            NorthwndContext context = new NorthwndContext();
+            Supplier supplier1 = context.Suppliers.FirstOrDefault(y=> y.SupplierId == supplier.SupplierId); 
+
+            supplier1.ContactName = supplier.ContactName;
+            supplier1.Phone = supplier.Phone;
+            supplier1.CompanyName = supplier.CompanyName;
+
+            context.SaveChanges();
+            return Ok("Güncelleme Başarılı");
+        
+        
+        
+        }
+
+    }     
+
 }
